605c360f4e2ab6459ff20d5587503315bb0b75c2
harigit3199 <hariharan3199@gmail.com>
2021-10-14 17:24:10 +0530

    Initial commit: Basic Java oops concepts and codes

---
+++ src/strdemo.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class strdemo {
+    public static void main(String[] args)
+    {
+        String companyName= new String("Apple");
+        String companyName1 = "Samsung";
+        System.out.println(companyName);
+        System.out.println(companyName1);
+    }
+}
